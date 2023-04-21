22812f0fff0f6876efe2cb257f9a17abc6e0ca97
yschristian <yubahwesc@gmail.com>
2023-04-21 17:48:47 +0200

    set up project

---
+++ src/app.ts
@@ -0,0 +1,59 @@
+import express, { Application } from "express";
+import mongoose from "mongoose";
+import compression from "compression";
+import helmet from "helmet"
+import cors from "cors";
+import morgan from "morgan";
+import Controller from "@/utils/interfaces/controller.interface";
+import ErrorMiddleware from "@/middleware/error.middleware";
+
+class App {
+    public express: Application;
+    public port: number;
+
+    constructor(controllers: Controller[], port: number) {
+        this.express = express();
+        this.port = port;
+
+        this.initialiseDatabaseConnection();
+        this.initialiseMiddleware();
+        this.initialiseController(controllers);
+        this.initialiseErrorHandling();
+
+    }
+
+    private initialiseMiddleware(): void {
+        this.express.use(helmet());
+        this.express.use(cors());
+        this.express.use(morgan("dev"));
+        this.express.use(express.json());
+        this.express.use(express.urlencoded({ extended: false }));
+        this.express.use(compression())
+    }
+
+    private initialiseController(controllers: Controller[]): void {
+        controllers.forEach((controller: Controller) => {
+            this.express.use("/api", controller.router)
+        })
+    }
+
+    private initialiseErrorHandling(): void {
+
+        this.express.use(ErrorMiddleware)
+    }
+
+    private initialiseDatabaseConnection(): void {
+
+        const { MONGO_URL } = process.env
+        mongoose.connect(`${MONGO_URL}`)
+    }
+    public listen(): void{
+        this.express.listen(this.port , () =>{
+            console.log(`app listening on port ${this.port}`);
+            
+        })
+    }
+
+}
+
+export default App
