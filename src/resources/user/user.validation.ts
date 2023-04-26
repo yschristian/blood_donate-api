d6d40d599f0f55325ac79b6525190c4c8388f041
yschristian <yubahwesc@gmail.com>
2023-04-26 11:58:40 +0200

    validations

---
+++ src/resources/user/user.validation.ts
@@ -0,0 +1,15 @@
+
+import Joi from "joi";
+
+const register = Joi.object({
+    username: Joi.string().max(30).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required()
+})
+
+const login = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required()
+})
+
+export default { register , login }
